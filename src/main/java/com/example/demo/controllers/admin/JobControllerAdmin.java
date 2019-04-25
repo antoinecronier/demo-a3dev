9c21e7c1a4b870a92a17fe50d260785f0cc3a2c2
Antoine Cronier <antoine.cronier@tactfactory.com>
2019-04-25 11:35:28 +0200

    save working changes

--- src/main/java/com/example/demo/controllers/admin/JobControllerAdmin.java
+++ src/main/java/com/example/demo/controllers/admin/JobControllerAdmin.java
@@ -33,7 +33,6 @@ public class JobControllerAdmin extends BaseAdminLinkedController<User> {
                     break;
                 }
             }
-            
             break;
 
         default:
